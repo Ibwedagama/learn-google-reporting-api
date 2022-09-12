dabe2d6013b68db2dfa7d29747e3cced74b0fbbd
Ibwedagama <wardana.wedagama@gmail.com>
2022-09-12 15:24:55 +0800

    feat: add vue-gtag plugins

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import router from './router'
 
 import './assets/main.css'
 
+import VueGtag from "vue-gtag"
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueGtag, {
+  config: { id: import.meta.env.VITE_GOOGLE_MEASUREMENT_ID }
+})
 
 app.mount('#app')
