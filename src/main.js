30ed4c7507d05a40a4117cd0c5246623bf689a29
Ibwedagama <wardana.wedagama@gmail.com>
2022-09-13 09:41:29 +0800

    config: add prettier eslint

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-import VueGtag from "vue-gtag"
+import VueGtag from "vue-gtag";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(VueGtag, {
-  config: { id: import.meta.env.VITE_GOOGLE_MEASUREMENT_ID }
-})
+  config: { id: import.meta.env.VITE_GOOGLE_MEASUREMENT_ID },
+});
 
-app.mount('#app')
+app.mount("#app");
